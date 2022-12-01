7f2c4846aa03767b899f14a57910718e9469c8d5
Simon Amby <mail@simonamby.dk>
2022-12-01 16:03:41 +0100

    Solution

---
+++ 2022/Day01.ps1
@@ -0,0 +1,20 @@
+## Source: https://adventofcode.com/2022/day/1
+[int]$highscore = 0
+[int]$temp_calories = 0
+
+#$sample = Get-Content -Path "..\Day01_Sample.csv"
+$sample = Get-Content -Path "..\Day01.csv"
+
+for ($i = 0; $i -lt $sample.Count; $i++) {
+    if ($sample[$i]) {
+        [int]$temp_calories = $temp_calories + [int]$sample[$i]
+    }
+    else {
+        if ($temp_calories -gt $highscore) {
+            $highscore = $temp_calories
+        }
+        $temp_calories = 0
+    }
+    
+}
+$highscore
